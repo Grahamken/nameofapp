3a91437e1f4848fb1a6b437dc2fde356d242e4ae
Graham <grahamken@hotmail.com>
2015-06-17 16:11:09 +0100

    bug fixes 3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,8 @@
 class StaticPagesController < ApplicationController
-	def landing_page
+	def index
+  	end
+  	
+  	def landing_page
 		@featured_product = Product.first
 	end
-
-  def index
-  end
 end
