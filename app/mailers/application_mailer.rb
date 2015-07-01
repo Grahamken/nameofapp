90996d85507e0c8ef271bd33d3f421550cfea645
Graham <grahamken@hotmail.com>
2015-07-01 14:08:46 +0100

    mailer v2

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,8 +2,5 @@ def thank_you
   @name = params[:name]
   @email = params[:email]
   @message = params[:message]
-  ActionMailer::Base.mail(:from => @email, 
-	  	:to => 'grahamken@hotmail.com', 
-	  	:subject => "A new contact form message from #{@name}", 
-	  	:body => @message).deliver
+  UserMailer.contact_form(@email, @name, @message).deliver
 end
