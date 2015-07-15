88fc5427901fc661c0c70c6c36e188b6b77ed67b
Graham <grahamken@hotmail.com>
2015-07-15 14:14:10 +0100

    rating 2.0

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
 
-  resources :products
+  resources :products do
+    resources :comments
+  end
   
   resources :orders, only: [:index, :show, :new, :create]
 
--- db/migrate/20150706144513_create_comments.rb
+++ db/migrate/20150706144513_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.references :user, index: true
       t.text :body
-      t.interger :rating
+      t.integer :rating
       t.references :product, index: true
       t.timestamps
     end
