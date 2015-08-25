fe41d0ac6b61ad606b71e58f6279717edda5e6ab
Graham <grahamken@hotmail.com>
2015-08-25 13:20:05 +0100

    stripe4

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,6 +3,8 @@ class PaymentsController < ApplicationController
 def create
   token = params[:stripeToken]
   product = Product.find(params[:product_id])
+  @email = params[:stripeEmail]
+
   # Create the charge on Stripe's servers - this will charge the user's card
   begin
     charge = Stripe::Charge.create(
